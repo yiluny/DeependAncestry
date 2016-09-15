74c7575c8028af63ea248330c13a4f69491c306d
yilun.yang <yilun.yang@redhotminute.com>
2016-09-15 23:09:16 +1000

    set up project and created phase one UI

---
+++ DeependAncestry/Models/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DeependAncestry.Models
+{
+    public class Person
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Gender { get; set; }
+
+        public int FatherId { get; set; }
+
+        public int MontherId { get; set; }
+
+        public int PlaceId { get; set; }
+
+        public int Level { get; set; }
+    }
+}
