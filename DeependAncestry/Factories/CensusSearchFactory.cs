1fefcaa6d49f4ae84bd08241ca5bea5be5c55be9
yilun.yang <yilun.yang@redhotminute.com>
2016-09-17 18:18:57 +1000

    add descebdants to search

--- DeependAncestry/Factories/CensusSearchFactory.cs
+++ DeependAncestry/Factories/CensusSearchFactory.cs
@@ -23,15 +23,31 @@ namespace DeependAncestry.Factories
         public SearchedResponse GetSearchResult(SearchedRequest req)
         {
             SearchedResponse res = new SearchedResponse();
+
             CensusData data = this.getCensusDataFromJson();
-            List<PersonViewModel> vmPeople = data.People.AsQueryable()
-                .Where(d => d.Name.Contains(req.Name))
-                .SelectMany(p => getParentsById(p.ID, new List<Person>(), data.People))
-                .Join(data.Places,
+
+            var query = data.People.AsQueryable()
+               .Where(d => d.Name.Contains(req.Name));
+
+            if (!string.IsNullOrWhiteSpace(req.Gender))
+            {
+                query = query.Where(d => d.Gender.ToLower() == req.Gender);
+            }
+            if (req.Family.ToLower() == "ancestors")
+            {
+                query = query.SelectMany(p => getParentsById(p.ID, p.Level, new List<Person>(), data.People));
+            }
+
+            if (req.Family.ToLower() == "descendants")
+            {
+                query = query.SelectMany(p => getChildrenById(p.ID, p.Gender, p.Level, new List<Person>(), data.People));
+            }
+
+            List<PersonViewModel> vmPeople = query.Join(data.Places,
                 people => people.PlaceId,
                 place => place.Id,
-                (people, place) => new PersonViewModel() { ID = people.ID, Name = people.Name, Gender = people.Gender, BirthPlace = place.Name,Level = people.Level })
-                .OrderBy(p=>p.Level).ToList();
+                (people, place) => new PersonViewModel() { ID = people.ID, Name = people.Name, Gender = people.Gender, BirthPlace = place.Name, Level = people.Level })
+                .OrderBy(p => p.Level).ToList();
 
             if (vmPeople != null && vmPeople.Count > 0)
             {
@@ -62,21 +78,44 @@ namespace DeependAncestry.Factories
         /// <param name="currentFamilies">Current family members found for current person</param>
         /// <param name="data">Total data set</param>
         /// <returns>Totle family members found</returns>
-        private List<Person> getParentsById(int personId, List<Person> currentFamilies, HashSet<Person> data)
+        private List<Person> getParentsById(int personId, int lvl, List<Person> currentFamilies, HashSet<Person> data)
         {
             List<Person> families = currentFamilies;
 
-            Person familyLookup = data.SingleOrDefault(p => p.ID == personId);
+            Person familyLookup = data.Where(p => p.Level <= lvl).SingleOrDefault(p => p.ID == personId);
             families.Add(familyLookup);
 
-            if (familyLookup.MotherId!= null)
+            if (familyLookup.MotherId != null)
             {
-                families = getParentsById(familyLookup.MotherId.Value, families, data);
+                families = getParentsById(familyLookup.MotherId.Value, familyLookup.Level, families, data);
             }
 
             if (familyLookup.FatherId != null)
             {
-                families = getParentsById(familyLookup.FatherId.Value, families, data);
+                families = getParentsById(familyLookup.FatherId.Value, familyLookup.Level, families, data);
+            }
+            return families;
+        }
+
+        /// <summary>
+        /// Get children based on monther id and father id
+        /// </summary>
+        /// <param name="personId">Current person ID</param>
+        /// <param name="currentFamilies">Current family members found for current person</param>
+        /// <param name="data">Total data set</param>
+        /// <returns>Totle family members found</returns>
+        private List<Person> getChildrenById(int personId, string gender, int lvl, List<Person> currentFamilies, HashSet<Person> data)
+        {
+            List<Person> families = currentFamilies;
+
+            List<Person> familyLookups = data.Where(p => p.Level >= lvl &&
+                                           (p.FatherId == personId && gender.ToLower() == "m"
+                                         || p.MotherId == personId && gender.ToLower() == "f")).ToList();
+            families.AddRange(familyLookups);
+
+            foreach (var member in familyLookups)
+            {
+                families = getChildrenById(member.ID, member.Gender, member.Level, families, data);
             }
             return families;
         }
